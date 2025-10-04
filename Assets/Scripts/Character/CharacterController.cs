a3ab251c802369ef07cf4b9708d5b5fe8f927b10
koheix <jj3vpo0705@gmail.com>
2025-10-04 23:21:38 +0900

    upudate: characterstatus for hunger recover, add: new assets

--- Assets/Scripts/Character/CharacterController.cs
+++ Assets/Scripts/Character/CharacterController.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 
 /*
 characterの動きを制御するためのコード
-歩数も記録する
+歩行距離を計測する
 */
 public class CharacterController : MonoBehaviour
 {
--- Assets/Scripts/Character/CharacterStatus.cs
+++ Assets/Scripts/Character/CharacterStatus.cs
@@ -21,6 +21,12 @@ public class CharacterStatus : MonoBehaviour
     //リンゴの数
     private int appleCount;
 
+    // 停止時のhunger回復用
+    [SerializeField] private float hungerRecoveryInterval = 0.1f; // 0.1秒ごと
+    [SerializeField] private int hungerRecoveryAmount = 1; // 回復量
+    private float idleTimer = 0f; // 止まっている時間をためて記録していく
+    private float previousWalkDistance = 0f; // 前の移動距離
+
     void Start()
     {
         // for test
@@ -41,6 +47,9 @@ public class CharacterStatus : MonoBehaviour
             PlayerPrefs.SetInt("hunger", 100);
             hunger = 100;
         }
+
+        //最初の歩行距離
+        previousWalkDistance = cc.GetTotalWalkDistance();
     }
 
     void Update()
@@ -48,30 +57,75 @@ public class CharacterStatus : MonoBehaviour
         Debug.Log(hunger);
         Debug.Log("playerpref hunger: " + PlayerPrefs.GetInt("hunger"));
         
+        //移動距離の取得
+        float currentWalkDistance = cc.GetTotalWalkDistance();
+
         // 単純に歩数が定数の閾値を超えたら体力を減らす
-        if (cc.GetTotalWalkDistance() > 1)
+        if (currentWalkDistance > 1)
         {
             //体力を減らす
             hunger = Mathf.Clamp(--hunger, 0, 100);
             PlayerPrefs.SetInt("hunger", hunger);
             cc.ResetWalkDistance();
             // statusのUIに反映
-            hungerGauge.fillAmount = hunger / 100f;
-            if (hunger > 60)
+            UpdateHungerUI();
+
+            // 歩いているのでタイマーをリセット
+            idleTimer = 0f;
+        }
+        else
+        {
+            // プレイヤーが止まっている
+            if(currentWalkDistance == previousWalkDistance)
             {
-                hungerGauge.color = Color.green;
+                // タイマー加算
+                idleTimer += Time.deltaTime;
+
+                Debug.Log("ifに入ってる");
+                Debug.Log(idleTimer);
+                // 1秒ごとにhungerを回復
+                if(idleTimer >= hungerRecoveryInterval)
+                {
+                    // hungerの反映
+                    hunger = Mathf.Clamp(hunger + hungerRecoveryAmount, 0, 100);
+                    PlayerPrefs.SetInt("hunger", hunger);
+                    UpdateHungerUI();
+
+                    // タイマーをリセット
+                    idleTimer = 0f;
+
+                }
             }
-            else if (hunger > 30) {
-                hungerGauge.color = Color.yellow;
-            }
-            else {
-                hungerGauge.color = Color.red;
+            else
+            {
+                // 移動しているならタイマーをリセット
+                idleTimer = 0f;
             }
         }
+
+        previousWalkDistance = currentWalkDistance;
 
         //リンゴの数をUIに反映
         appleCount = PlayerPrefs.GetInt("appleCount", 0);
         appleCountUI.text = appleCount.ToString();
+    }
+
+    // hungerのUIを更新するメソッド
+    private void UpdateHungerUI()
+    {
+        hungerGauge.fillAmount = hunger / 100f;
+        if (hunger > 60)
+        {
+            hungerGauge.color = Color.green;
+        }
+        else if (hunger > 30)
+        {
+            hungerGauge.color = Color.yellow;
+        }
+        else
+        {
+            hungerGauge.color = Color.red;
+        }
     }
 
     void FixedUpdate()
