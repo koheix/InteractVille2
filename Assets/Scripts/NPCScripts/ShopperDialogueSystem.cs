946e575d6a462a3033d099eedd9a5df247a1b934
koheix <grapeoxygen@gmail.com>
2025-10-13 16:57:07 +0900

    add: shopper npc and shopscene

---
+++ Assets/Scripts/NPCScripts/ShopperDialogueSystem.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class ShopperDialogueSystem : DialogueSystem
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    @Override
+    public void StartDialogue()
+    {
+        base.StartDialogue();
+        // ここにShopperDialogueSystem固有の処理を追加
+    }   
+}
