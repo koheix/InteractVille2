7c85efad24135c0a8c0723a9f105c49c852be604
koheix <grapeoxygen@gmail.com>
2025-12-28 11:44:59 +0900

    update: 初回のタイトル画面に移動するまでのキャンバスを追加。こちらの画像を後で編集する。

--- Assets/Scripts/CommonUISceneScripts/CommonUICanvas.cs
+++ Assets/Scripts/CommonUISceneScripts/CommonUICanvas.cs
@@ -12,12 +12,16 @@ public class CommonUICanvas : MonoBehaviour
     private Canvas rootCanvas;
     private Button return2TitleButton;
 
+    [Header("最初のロード画面のCanvas")]
+    [SerializeField] private Canvas firstLoadCanvas;
+
     // singleton
     public static CommonUICanvas Instance { get; private set; }
 
     private void Awake()
     {
         rootCanvas = GetComponent<Canvas>();
+        firstLoadCanvas.enabled = true;
         // rootCanvasを非表示にしておく
         // rootCanvas.enabled = false;
         // Singleton
@@ -36,13 +40,18 @@ public class CommonUICanvas : MonoBehaviour
         {
             QuitManager.Instance.RequestReturn2Title();
         });
-
         // シーン変化を監視
         SceneManager.activeSceneChanged += OnSceneChanged;
     }
 
     private void OnSceneChanged(Scene pre, Scene next)
     {
+        // 最初のロード画面を非表示にする
+        // if (firstLoadCanvas != null && firstLoadCanvas.enabled)
+        if (firstLoadCanvas != null && pre.name == "CommonUIScene")
+        {
+            firstLoadCanvas.enabled = false;
+        }
         // タイトルシーン名に応じて切り替え
         if (next.name == "TitleScene") 
         {
